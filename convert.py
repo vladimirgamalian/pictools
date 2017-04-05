9cdc8c7c5397a710369150e9a27ef1e69bb8e4c6
Vladimir Gamalian <vladimir.gamalian@gmail.com>
2017-04-05 22:27:20 +0700

    Add convert script

---
+++ convert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import click
+import os
+from PIL import Image
+from utils import get_file_list
+
+
+@click.command()
+@click.argument('path', type=click.Path(exists=True))
+@click.option('-f', '--file-format', required=True, type=click.Choice(['png', 'bmp']))
+def convert(path, file_format):
+    """
+    Convert image(s) to PNG or BMP format.
+    """
+    for file_name in get_file_list(path):
+        new_file_name = os.path.splitext(file_name)[0] + '.' + file_format
+        if new_file_name != file_name:
+            img = Image.open(file_name)
+            img.save(new_file_name)
+            os.remove(file_name)
+
+
+if __name__ == '__main__':
+    convert()
