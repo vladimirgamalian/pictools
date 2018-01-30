a6cfebfb14631dc0dbc5e7ed4f9168ae8d4193fc
Vladimir Gamalyan <vladimir.gamalian@gmail.com>
2018-01-30 16:00:26 +0300

    Add --alphabet option to renamer script

--- renamer.py
+++ renamer.py
@@ -9,8 +9,10 @@ from utils import TempDir, natural_sort_key
 @click.command()
 @click.argument('path', type=click.Path(exists=True, file_okay=False))
 @click.option('--start', type=click.IntRange(min=0), prompt=True)
-def renamer(path, start):
-    files = sorted([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.lower().endswith('.png')], key=natural_sort_key)
+@click.option('-a', '--alphabet', is_flag=True, help='alphabet sort')
+def renamer(path, start, alphabet):
+    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.lower().endswith('.png')]
+    files = sorted(files) if alphabet else sorted(files, key=natural_sort_key)
     files = [(f, ('%04d' % (start + i)) + os.path.splitext(f)[1]) for i, f in enumerate(files)]
     with TempDir(path) as t:
         for f in files:
